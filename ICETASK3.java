13bbf133cfa429f7e479e6242b7dea52b3de8a56
ST10385561 <128820514+ST10385561@users.noreply.github.com>
2024-05-22 23:06:41 +0200

    Add files via upload

---
+++ ICETASK3.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ice.task.pkg3;
+
+/**
+ *
+ * @author Wavhu Budeli
+ */
+public class ICETASK3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    StringBuilder str = new StringBuilder("Hello");
+    System.out.println(str.toString());
+    str.append(" World!");
+    System.out.println(str.toString());
+    }
+    StringBuilder s1 = new StringBuilder();
+    
+    private int capacity;
+    StringBuilder s2 = new StringBuilder(capacity);
+    
+    private int sequence;
+    StringBuilder s3 = new StringBuilder(sequence);
+    
+    
+    
+}
